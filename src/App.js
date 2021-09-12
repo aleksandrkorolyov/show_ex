8b633b67e1e383ba128f5b45103197d94f8cdc50
Aleksandr Korolyov <akorolyov92@gmail.com>
2021-09-12 13:31:39 +0300

    add_kva

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
         </p>
       <div>Hi, git!!!</div>
+      <div>kva-kva</div>
         <a
           className="App-link"
           href="https://reactjs.org"
